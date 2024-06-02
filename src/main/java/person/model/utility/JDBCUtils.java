f02bdcfbd19d4056813098f3444f59553dfe7513
tijanaignjatov <148060203+tijanaignjatov@users.noreply.github.com>
2024-06-02 16:26:18 +0200

    dodati neki upiti

--- src/main/java/person/model/utility/JDBCUtils.java
+++ src/main/java/person/model/utility/JDBCUtils.java
@@ -1,7 +1,6 @@
 package person.model.utility;
 
-import person.model.base.Korisnici;
-import person.model.base.Server;
+import person.model.base.*;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -45,6 +44,74 @@ public class JDBCUtils {
         }
         return people;
     }
+    public static List<Objekat> selectObjekatFromZus(){
+        List<Objekat> objekti = new ArrayList<>();
+        String query = "select objekat_id,naziv, vrsta from zus.objekti";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int objekat_id = resultSet.getInt(1);
+                String naziv = resultSet.getString(2);
+                String vrsta = resultSet.getString(3);
+                Objekat objekat = new Objekat(objekat_id,naziv, vrsta);
+                objekti.add(objekat);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+      return objekti;
+    }
+
+    public static List<Objekat> selectDetaljanObjekatFromZus(){
+        List<Objekat> objekti = new ArrayList<>();
+        String query = "select * from zus.objekti o join zus.misije m on(o.objekat_id = m.objekaat_id) ";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int objekat_id = resultSet.getInt(1);
+                String naziv = resultSet.getString(2);
+                String vrsta = resultSet.getString(3);
+                int udaljenost_od_planete = resultSet.getInt(4);
+                int najniza_temperatura = resultSet.getInt(5);
+                int najvisa_temperatura = resultSet.getInt(6);
+                int kiseonik = resultSet.getInt(7);
+                String drugi_gas = resultSet.getString(8);
+                int kolicina_drugog_gasa = resultSet.getInt(9);
+                int visina = resultSet.getInt(10);
+                int brzina_orbitiranja = resultSet.getInt(11);
+                int broj_umrlih = resultSet.getInt(12);
+                Objekat objekat = new Objekat(objekat_id,naziv, vrsta);
+                objekti.add(objekat);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return objekti;
+    }
+
+    public static List<StambeniObjekat> selectStambeniObjekatFromZus() {
+        List<StambeniObjekat> stambeniObjekti = new ArrayList<>();
+        String query = "select * from zus.stambeni_objekti";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int stambeniObjekatId = resultSet.getInt(1);
+                String vrstaStambenogObjekta = resultSet.getString(2);
+                int kvadratura = resultSet.getInt(3);
+                int brojStanara = resultSet.getInt(4);
+                boolean dostuonost = resultSet.getBoolean(5);
+                StambeniObjekat stambeni = new StambeniObjekat(stambeniObjekatId, vrstaStambenogObjekta, kvadratura, brojStanara, dostuonost);
+                stambeniObjekti.add(stambeni);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return stambeniObjekti;
+    }
+
 
     public static List<Korisnici> selectFromPerson(String firstName, String lastName, String yearOfBirth) {
         List<Korisnici> oldPeople = selectAllFromZus();
